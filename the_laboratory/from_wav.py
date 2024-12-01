483c12d693021cb49d268054c34b35d7667401b2
erfansamandarian <mail@samandarian.org>
2024-12-01 20:26:19 +1100

    r&d

---
+++ the_laboratory/from_wav.py
@@ -0,0 +1,51 @@
+# converts from persian to english using vosk and deep_translator
+
+import wave
+import json
+from vosk import Model, KaldiRecognizer
+from deep_translator import GoogleTranslator
+
+model_path = "models/vosk-model-small-fa-0.42"
+
+model = Model(model_path)
+
+# should be 16000 Hz 16-bit PCM
+wf = wave.open("test.wav", "rb")
+
+recognizer = KaldiRecognizer(model, wf.getframerate())
+
+translator = GoogleTranslator(source='auto', target='en')
+
+with open("leslie.txt", "w") as output_file:
+    while True:
+        data = wf.readframes(4000)
+        if len(data) == 0:
+            break
+        if recognizer.AcceptWaveform(data):
+            result = recognizer.Result()
+            try:
+                result_json = json.loads(result)
+                print("raw result:", result_json)
+                if 'text' in result_json:
+                    cleaned_text = result_json['text'].replace('\u200c', '')
+                    translated_text = translator.translate(cleaned_text)
+                    output_file.write(f"English: {translated_text}\n")
+                else:
+                    print("no 'text' key in partial result")
+            except json.JSONDecodeError:
+                print("error decoding the JSON result")
+
+    final_result = recognizer.FinalResult()
+    try:
+        final_result_json = json.loads(final_result)
+        print("final result:", final_result_json)
+        if 'text' in final_result_json:
+            cleaned_text = final_result_json['text'].replace('\u200c', '')
+            translated_text = translator.translate(cleaned_text)
+            output_file.write(f"{translated_text}\n")
+        else:
+            print("no 'text' key in final result")
+    except json.JSONDecodeError:
+        print("json decode error")
+
+print("done")
